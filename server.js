a493e7e22391b88f22a451376fe1432344d10ee8
froggal <doeon111@naver.com>
2023-06-25 19:39:53 +0900

    bug...

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const port = 8080;
 
 // 데이터베이스 연결 설정
 const db = new sqlite3.Database('books.db');
+
 // public static 설정
 app.use(express.static('public'));
-app.use(express.static('public', { 'Content-Type': 'text/javascript' }));
-app.use(express.static('public', { 'Content-Type': 'text/css' }))
-app.use(express.static(__dirname + '/public'));
 
+// JSON 데이터 파싱을 위한 미들웨어 설정
+app.use(express.json());
 
 // 라우트 설정
 app.get('/books', (req, res) => {
@@ -27,45 +27,49 @@ app.get('/books', (req, res) => {
 });
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-  });
+  res.sendFile(__dirname + '/index.html');
+});
 
-  // 라우트 설정 - 책 추가
+// 라우트 설정 - 책 추가
 app.post('/books/add', (req, res) => {
-    const { title, author, location } = req.body;
-  
-    // 데이터베이스에 새로운 책 추가
-    const sql = `INSERT INTO books (title, author, location) VALUES (?, ?, ?)`;
-    db.run(sql, [title, author, location], function (err) {
-      if (err) {
-        console.error(err);
-        res.status(500).send('Internal Server Error');
-      } else {
-        // 책 추가 성공
-        res.send('Book added successfully');
-      }
-    });
-  });
-  
-// 이전에 작성한 코드...
+  const { title, author, location } = req.body;
 
-// 라우트 설정
+  // 데이터베이스에 새로운 책 추가
+  const sql = `INSERT INTO books (title, author, location) VALUES (?, ?, ?)`;
+  db.run(sql, [title, author, location], function (err) {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Internal Server Error');
+    } else {
+      // 책 추가 성공
+      res.send('Book added successfully');
+    }
+  });
+});
+
+// 라우트 설정 - 책 검색
 app.get('/books/search', (req, res) => {
-    const searchQuery = req.query.q;
-  
-    // 데이터베이스에서 검색 쿼리 실행
-    const sql = `SELECT * FROM books WHERE title LIKE '%${searchQuery}%' OR author LIKE '%${searchQuery}%'`;
-    db.all(sql, (err, rows) => {
-      if (err) {
-        console.error(err);
-        res.status(500).send('Internal Server Error');
-      } else {
-        res.json(rows);
-      }
-    });
+  const searchQuery = req.query.q;
+
+  // 데이터베이스에서 검색 쿼리 실행
+  const sql = `SELECT * FROM books WHERE title LIKE '%${searchQuery}%' OR author LIKE '%${searchQuery}%'`;
+  db.all(sql, (err, rows) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Internal Server Error');
+    } else {
+      res.json(rows);
+    }
   });
-  
-  // 서버 시작
-  app.listen(port, () => {
-    console.log(`Server listening at http://localhost:${port}`);
-  });
+});
+
+// 서버 시작
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
+
+// 데이터베이스 연결 종료
+process.on('SIGINT', () => {
+  db.close();
+  process.exit();
+});
